c789c4d2efcf79854a66c3dc8346609b27524fdb
ryemountain <john@bohman.it>
2024-06-19 18:18:19 +0200

    Implemented button for clearing grid and inputting a new size of grid.

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
+//Define the grid function
+function resetGrid(gridSize) {
 
-    //create the columns
-    const colDiv = document.createElement("div");
-    colDiv.classList.add("column");
-    container.appendChild(colDiv);
+    const containerElements = container.querySelectorAll("*");
 
-    //create the rows
-    for (let i = 0; i < 16; i++) {
-        const rowDiv = document.createElement("div");
-        rowDiv.classList.add("gridbox");
-        colDiv.appendChild(rowDiv);
+    containerElements.forEach(x => x.remove());
+
+    for (let i = 0; i < gridSize; i++) {
+
+        //create the columns
+        const colDiv = document.createElement("div");
+        colDiv.classList.add("column");
+        container.appendChild(colDiv);
+    
+        //create the rows
+        for (let i = 0; i < gridSize; i++) {
+            const rowDiv = document.createElement("div");
+            rowDiv.classList.add("gridbox");
+    
+            rowDiv.addEventListener("mouseenter", () => {
+                rowDiv.classList.add("gridbox-hovered");
+            });
+            colDiv.appendChild(rowDiv);
+        }
     }
 }
+
+//Define the function for resetting the grid by clicking button
+
+const btn = document.querySelector(".resetBtn");
+
+btn.addEventListener("click", () => {
+    const newSize = Number(prompt("Write the number of columns for your grid, for example 20 (the number " +
+    "of rows will be the same)"));
+
+    if (newSize > 100) {
+        alert("The maximum size of the grid is 100x100!");
+    }
+    else
+    {
+        resetGrid(newSize)
+    }
+})
+
+//Create the initial grid 
+resetGrid(16);
